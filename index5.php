80b3d641cca879d71d6277359db97fc9396c3367
zhouyu537 <13594131056@qq.com>
2024-11-20 19:56:31 +0800

    合并后修改了index5文件

--- index5.php
+++ index5.php
@@ -3,13 +3,8 @@
 echo "this is index5.php";
 echo "this is index5.php";
 echo "this is index5.php";
-echo "this is index5.php";
-echo "this is index5.php";
-echo "this is index5.php";
 // phpinfo();
 
-echo "本地是这样加的"
-echo "本地是这样加的"
 echo "本地是这样加的"
 echo "本地是这样加的"
 echo "本地是这样加的"
